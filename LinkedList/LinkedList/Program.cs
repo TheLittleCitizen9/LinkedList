8029738287db0127369adb879373f43c502ed72c
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-11 16:40:57 +0300

    deleted error row from program

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -12,7 +12,6 @@ namespace LinkedList
             linkedList.AddFirst(2);
             linkedList.AddLast(new LinkedListNode<int>(4));
             linkedList.AddLast(new LinkedListNode<int>(6));
-            linkedList.AddLast(linkedList.Find(4));
 
             CirclularLinkedListValidator circlularLinkedListValidator = new CirclularLinkedListValidator();
             Console.WriteLine(circlularLinkedListValidator.IsCircular<int>(linkedList));
